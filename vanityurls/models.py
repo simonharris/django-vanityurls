0e5372341ee7d1307206ed5f35e8b438d7056a59
Simon H <github@pointbeing.net>
2024-12-18 16:21:43 +0000

    Stop manage.py complaining

--- vanityurls/models.py
+++ vanityurls/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class VanityUrl(models.Model):
 
     id = models.AutoField(primary_key=True)
-    vanity_url = models.CharField(max_length=256,  default='', null=False, unique=True)
+    vanity_url = models.CharField(max_length=255,  default='', null=False, unique=True)
     target = models.CharField(max_length=512, default='', blank=False, null=False)
 
     class Meta:
